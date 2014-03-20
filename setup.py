afb537642ddc2b4385c54992dd51dbf28c935b68
Dan Lecocq <dan@moz.com>
2014-03-20 10:56:25 -0700

    Version bump and setup cleanup

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from distutils.core import setup
 
-ext_files = ["pyreBloom/bloom.c"]
+ext_files = ['pyreBloom/bloom.c']
 
 kwargs = {}
 
 try:
     from Cython.Distutils import build_ext
     from Cython.Distutils import Extension
-    print "Building from Cython"
-    ext_files.append("pyreBloom/pyreBloom.pyx")
+    print 'Building from Cython'
+    ext_files.append('pyreBloom/pyreBloom.pyx')
     kwargs['cmdclass'] = {'build_ext': build_ext}
 except ImportError:
     from distutils.core import Extension
-    ext_files.append("pyreBloom/pyreBloom.c")
-    print "Building from C"
+    ext_files.append('pyreBloom/pyreBloom.c')
+    print 'Building from C'
 
-ext_modules = [Extension("pyreBloom", ext_files, libraries=['hiredis'])]
+ext_modules = [Extension('pyreBloom', ext_files, libraries=['hiredis'])]
 
 setup(
     name = 'pyreBloom',
-    version = "1.0.1",
-    author = "Dan Lecocq",
-    author_email = "dan@seomoz.org",
-    license = "MIT License",
+    version = '1.0.2',
+    author = 'Dan Lecocq',
+    author_email = 'dan@seomoz.org',
+    license = 'MIT License',
     ext_modules = ext_modules,
     classifiers = [
         'Intended Audience :: Developers',
